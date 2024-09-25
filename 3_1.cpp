7089473e89e898cfe446bff46c3dc504afb4eb7d
freakspacecadet <125437248+freakspacecadet@users.noreply.github.com>
2024-09-25 10:00:55 +0200

    Create 3_1.cpp

---
+++ 3_1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+bool f(int x, int y) {
+    while (y != 0) {
+       if (x == y) {
+          return true;
+       }
+       y = (y - (y%2))/2;
+    }
+}
+
+int main()
+    int x;
+    int y;
+	for (int i = 0; i < 1000; i++) {
+       cin >> x >> y;
+       if (f(x, y) == true) {
+          cout << x << y << endl;
+       }
+    }
+	return 0;
+}
